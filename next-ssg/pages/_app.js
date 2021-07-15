6d4b6524006e11ca8dc5c0b52606ffaf0d9d9bec
Suguru Murakami <sugumura@gmail.com>
2021-07-16 01:08:14 +0900

    Add title

--- next-ssg/pages/_app.js
+++ next-ssg/pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>SSG Next Sample</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
